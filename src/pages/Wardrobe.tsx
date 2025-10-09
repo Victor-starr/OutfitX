1f26d343820d17ce454a7702d0ac378a172a05dc
Victor-starr <v94625230@gmail.com>
2025-10-09 16:42:52 +0300

    Refactor Wardrobe component to use useWardrobe hook and remove unused code

--- src/pages/Wardrobe.tsx
+++ src/pages/Wardrobe.tsx
@@ -1,51 +1,24 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import AsideNav from "@/components/AsideNav";
 import { useNavigate } from "react-router";
-import { useAuth } from "react-oidc-context";
 import { ItemCard, LoadingItemCard } from "@/components/WardrobeItemCard";
-import useApi from "@/hook/UseApi";
-import { WardrobeData } from "@/data/Mocks";
-import type { WardrobeItem } from "@/types/items_types";
-
-const DEV: boolean = import.meta.env.VITE_DEV === "true";
+import useWardrobe from "@/hook/useWardrobe";
 
 function Wardrobe() {
   const navigate = useNavigate();
-  const auth = useAuth();
-  const api = useApi();
+  const { loading, result } = useWardrobe();
 
-  const [data, setData] = useState<WardrobeItem[]>([]);
   const [activeCategory, setActiveCategory] = useState<string>("All");
-  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const getFilteredItems = () => {
     if (activeCategory === "All") {
-      return data;
+      return result.data;
     }
 
-    return data.filter((item) => item.category === activeCategory);
+    return result.data.filter((item) => item.category === activeCategory);
   };
 
   const filteredData = getFilteredItems();
-
-  useEffect(() => {
-    async function fetchData() {
-      setIsLoading(true);
-      if (DEV) {
-        await new Promise((resolve) => setTimeout(resolve, 500));
-        setData(WardrobeData);
-        console.log("Using mock data for wardrobe");
-      } else {
-        const res = await api.get("/clothes/getwardrobe");
-        if (res.status === 200) {
-          setData(res.data);
-          console.table(res.data);
-        }
-      }
-      setIsLoading(false);
-    }
-    fetchData();
-  }, [auth.user?.access_token]);
 
   return (
     <main className="flex flex-col bg-bg py-8 pb-20 min-h-[calc(100vh-120px)]">
@@ -58,7 +31,7 @@ function Wardrobe() {
           onCategoryChange={setActiveCategory}
         />
         <div className="flex-1 gap-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 auto-rows-min pr-15 pl-5 w-full">
-          {isLoading ? (
+          {loading ? (
             Array.from({ length: 6 }).map((_, index) => (
               <LoadingItemCard key={index} />
             ))

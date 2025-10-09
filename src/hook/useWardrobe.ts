553e2df16eb5c798295294627e4b821595fa1a99
Victor-starr <v94625230@gmail.com>
2025-10-09 18:11:27 +0300

    Update useWardrobe hook to improve data handling and error parsing

--- src/hook/useWardrobe.ts
+++ src/hook/useWardrobe.ts
@@ -1,8 +1,10 @@
 import type { WardrobeItem } from "@/types/items_types";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import useApi from "./UseApi";
+import { WardrobeData, DetailData } from "@/data/Mocks";
+import { parseAxiosErrorDetails } from "@/utils/parseAxiosErrorDetails";
 import { useAuth } from "react-oidc-context";
-import { WardrobeData } from "@/data/Mocks";
+import { useNavigate } from "react-router";
 
 type ResType = {
   message: string;
@@ -12,23 +14,32 @@ type ResType = {
 interface UseWardrobeReturn {
   loading: boolean;
   result: ResType;
+  fetchItem: () => Promise<void>;
+  fetchData: () => Promise<void>;
+  handleDeleteItem: () => Promise<void>;
+}
+interface useWardrobeProps {
+  itemId?: string | undefined;
 }
 
-export default function useWardrobe(): UseWardrobeReturn {
-  const auth = useAuth();
+export default function useWardrobe({
+  itemId,
+}: useWardrobeProps = {}): UseWardrobeReturn {
   const api = useApi();
+  const auth = useAuth();
+  const navigate = useNavigate();
   const [loading, setIsLoading] = useState<boolean>(true);
   const [result, setResult] = useState<ResType>({
-    message: "",
+    message: "Not loaded",
     data: [],
     status: 0,
   });
 
   const DEV: boolean = import.meta.env.VITE_DEV === "true";
 
-  useEffect(() => {
-    async function fetchData() {
-      setIsLoading(true);
+  async function fetchData() {
+    setIsLoading(true);
+    try {
       if (DEV) {
         await new Promise((resolve) => setTimeout(resolve, 500));
         setResult({
@@ -36,24 +47,100 @@ export default function useWardrobe(): UseWardrobeReturn {
           data: WardrobeData,
           status: 200,
         });
-        console.log("Using mock data for wardrobe");
+        console.log("Using mock data for wardrobe", WardrobeData);
       } else {
         const res = await api.get("/clothes/getwardrobe");
-        if (res.status === 200) {
-          setResult({
-            message: res.statusText,
-            data: res.data,
-            status: res.status,
-          });
-          console.table(res.data);
-        }
+        setResult({
+          message: res.data.message,
+          data: res.data.data,
+          status: res.status,
+        });
       }
+    } catch (err) {
+      const { message, status } = parseAxiosErrorDetails(err);
+      console.error("Failed to fetch wardrobe:", { message, status });
+      setResult({
+        message: message,
+        data: [],
+        status: status,
+      });
+    } finally {
       setIsLoading(false);
     }
-    fetchData();
-  }, [auth.user?.access_token]);
+  }
+
+  async function fetchItem() {
+    setIsLoading(true);
+    try {
+      if (DEV) {
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        setResult({
+          message: "Mock data fetched",
+          data: [DetailData],
+          status: 200,
+        });
+        console.log("Using mock data for item details", DetailData);
+      } else {
+        const res = await api.get(`/clothes/${itemId}`);
+        setResult({
+          message: res.data.message,
+          data: [res.data.data],
+          status: res.status,
+        });
+      }
+      setIsLoading(false);
+    } catch (err) {
+      const { message, status } = parseAxiosErrorDetails(err);
+      console.error("Failed to fetch item details:", { message, status });
+      setResult({
+        message: message,
+        data: [],
+        status: status,
+      });
+      setIsLoading(false);
+    }
+  }
+
+  const handleDeleteItem = async () => {
+    if (auth.user?.profile?.sub !== result.data[0]?.userId) {
+      console.error("You are not authorized to delete this item.");
+      return;
+    }
+    try {
+      setIsLoading(true);
+      if (DEV) {
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        console.log("Mock delete item with ID:", itemId);
+      } else {
+        const res = await api.delete(`/clothes/${itemId}`, {
+          data: { itemId },
+        });
+        setResult({
+          message: res.data.message,
+          data: [],
+          status: res.status,
+        });
+      }
+      navigate("/wardrobe");
+      console.log("Deleting item with ID:", itemId);
+    } catch (err) {
+      const { message, status } = parseAxiosErrorDetails(err);
+      console.error("Failed to delete item:", { message, status });
+      setResult({
+        message,
+        data: [],
+        status,
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return {
     loading,
     result,
+    fetchItem,
+    fetchData,
+    handleDeleteItem,
   };
 }

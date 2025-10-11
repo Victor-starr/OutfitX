2f3c1b0b15929c78b0ca468f2263451ccc6f8d66
Victor-starr <v94625230@gmail.com>
2025-10-11 15:43:24 +0300

    Add routing for Outfits in App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import AuthGuard from "@/guards/AuthGuard";
 import Wardrobe from "@/pages/Wardrobe";
 import Details from "@/pages/Details";
 import Edit from "./pages/Edit";
+import Outfits from "./pages/Outfits";
 
 export default function App() {
   return (
     <Routes>
       <Route index element={<Home />} />
+      <Route path="/outfits" element={<Outfits />} />
       <Route path="/wardrobe" element={<AuthGuard />}>
         <Route index element={<Wardrobe />} />
         <Route path=":itemId">

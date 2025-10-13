63cf3bf9e099ce1c4d704a2869860a60da8958ae
Victor-starr <v94625230@gmail.com>
2025-10-14 00:27:39 +0300

    Refactor: Move pages to respective directories

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router";
 import NotFound from "@/pages/NotFound";
 import Home from "@/pages/Home";
-import CreateClothes from "@/pages/CreateClothes";
+import CreateClothes from "@/pages/clothes/CreateClothes";
 import AuthGuard from "@/guards/AuthGuard";
-import Wardrobe from "@/pages/Wardrobe";
-import Details from "@/pages/Details";
-import Edit from "./pages/Edit";
-import Outfits from "./pages/Outfits";
-import CreateOutfit from "./pages/CreateOutfit";
+import Wardrobe from "@/pages/clothes/Wardrobe";
+import ClothesDetails from "@/pages/clothes/ClothesDetails";
+import Edit from "./pages/clothes/ClothesEdit";
+import Outfits from "./pages/outfits/Outfits";
+import CreateOutfit from "./pages/outfits/CreateOutfit";
+import OutfitDetails from "./pages/outfits/OutfitDetail";
 
 export default function App() {
   return (
@@ -16,12 +17,13 @@ export default function App() {
       <Route path="/outfits" element={<AuthGuard />}>
         <Route index element={<Outfits />} />
         <Route path="create" element={<CreateOutfit />} />
+        <Route path=":outfitId" element={<OutfitDetails />} />
       </Route>
       <Route path="/wardrobe" element={<AuthGuard />}>
         <Route index element={<Wardrobe />} />
         <Route path="create" element={<CreateClothes />} />
         <Route path=":itemId">
-          <Route index element={<Details />} />
+          <Route index element={<ClothesDetails />} />
           <Route path="edit" element={<Edit />} />
         </Route>
       </Route>

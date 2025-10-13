128db9a5777be2457b80c4dd72736195ce2fa4c9
Victor-starr <v94625230@gmail.com>
2025-10-14 00:28:33 +0300

    feat: Implement ClothesDetails page

---
+++ src/pages/clothes/ClothesDetails.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import { useAuth } from "react-oidc-context";
+import {
+  ItemCardDetail,
+  LoadingItemCardDetail,
+} from "@/components/WardrobeItemCard";
+import useWardrobe from "@/hook/useWardrobe";
+import NavigateBack from "@/components/NavigateBack";
+
+function ClothesDetails() {
+  const auth = useAuth();
+  const { itemId } = useParams<{ itemId: string }>();
+  const { loading, result, fetchItem, handleDeleteItem } = useWardrobe({
+    itemId,
+  });
+
+  useEffect(() => {
+    fetchItem();
+  }, [auth.user?.access_token, itemId]);
+
+  return (
+    <main className="relative flex flex-col items-center bg-bg py-8 h-[85vh] overflow-y-auto">
+      <h1 className="mb-6 font-bold text-title text-2xl md:text-3xl lg:text-4xl text-center">
+        Wardrobe Item Details
+      </h1>
+      <NavigateBack url={-1} />
+
+      {loading ? (
+        <LoadingItemCardDetail />
+      ) : result.data === null || result.status !== 200 ? (
+        <p className="top-1/2 absolute flex justify-center items-center col-span-3 w-auto text-muted text-xl lg:text-3xl text-center">
+          No items found in this category.
+        </p>
+      ) : (
+        <ItemCardDetail
+          item={result.data[0]}
+          onDelete={handleDeleteItem}
+          isLoading={loading}
+        />
+      )}
+    </main>
+  );
+}
+
+export default ClothesDetails;

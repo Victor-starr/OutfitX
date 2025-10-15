ab10e43d43ee380d4852983cb40a75c2c5e8d3fb
Victor-starr <v94625230@gmail.com>
2025-10-16 00:09:36 +0300

    Refactor: Centralize Outfit Creation/Detail UI with shared components

--- src/pages/outfits/OutfitDetail.tsx
+++ src/pages/outfits/OutfitDetail.tsx
@@ -2,18 +2,13 @@ import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router";
 import useOutfits from "@/hook/useOutfit";
 import useWardrobe from "@/hook/useWardrobe";
-import TagsFilter, { LoadingTagsFilter } from "@/components/TagsFilter";
 import OutfitSubmissionForm from "@/components/OutfitSubmissionForm";
-import { ItemCard } from "@/components/WardrobeItemCard";
 import NavigateBack from "@/components/NavigateBack";
 import DeletePopUp from "@/components/DeletePopUp";
 import Button from "@/components/Button";
 import type { WardrobeItem } from "@/types/clothing_types";
-import { TbChefHat, TbJacket, TbShoe } from "react-icons/tb";
-import { IoShirtOutline } from "react-icons/io5";
-import { PiPants } from "react-icons/pi";
-import { FiWatch } from "react-icons/fi";
-import Human from "@/assets/human.svg";
+import OutfitPickerSection from "@/components/OutfitPickerSection";
+import AsideClothingPickerSection from "@/components/AsideClothingPickerSection";
 
 function OutfitDetails() {
   const tagsContainerRef = useRef<HTMLDivElement>(null);
@@ -80,27 +75,6 @@ function OutfitDetails() {
     return Array.from(new Set(allTags));
   };
 
-  const filterItemsByTag = (tag: string) => {
-    setSelectedTag(tag);
-
-    const baseList = selectedCategory
-      ? ClothesResult.data.filter(
-          (item) =>
-            item.category.toLowerCase() === selectedCategory.toLowerCase()
-        )
-      : ClothesResult.data;
-
-    if (tag === "All") {
-      setFilteredItems(baseList);
-    } else {
-      setFilteredItems(
-        baseList.filter((item) =>
-          item.tags.some((t) => t.toLowerCase() === tag.toLowerCase())
-        )
-      );
-    }
-  };
-
   const handleSelectCategory = (category: WardrobeItem["category"]) => {
     setShowUpdateButton(true);
     setSelectedCategory(category);
@@ -156,173 +130,55 @@ function OutfitDetails() {
       ) : (
         <>
           <section className="flex flex-row justify-center items-center gap-15 px-10 rounded-2xl w-[90%] h-[65vh]">
-            <article className="relative flex justify-center items-center p-6 w-[35%] h-full">
-              <img
-                src={Human}
-                alt="Human figure"
-                className="z-1 w-full h-full"
-              />
-              <div className="top-0 left-0 z-2 absolute flex flex-col justify-center items-center gap-10 w-full h-full">
-                <div className="flex flex-row gap-15 ml-35">
-                  <button
-                    onClick={() => handleSelectCategory("Head")}
-                    className="bg-white/45 p-6 border-2 border-surface border-dashed rounded-2xl"
-                  >
-                    {ClothesLoading ? (
-                      <div className="border-white border-b-2 rounded-full w-8 h-8 animate-spin"></div>
-                    ) : outfitSections.Head ? (
-                      <img
-                        src={outfitSections.Head.imageURL}
-                        alt={outfitSections.Head.name}
-                        className="rounded-full w-8 h-8 object-cover"
-                      />
-                    ) : (
-                      <TbChefHat color="white" size={30} />
-                    )}
-                  </button>
-                  <button
-                    onClick={() => handleSelectCategory("Accessories")}
-                    className="bg-white/45 p-6 border-2 border-surface border-dashed rounded-2xl"
-                  >
-                    {ClothesLoading ? (
-                      <div className="border-white border-b-2 rounded-full w-8 h-8 animate-spin"></div>
-                    ) : outfitSections.Accessories ? (
-                      <img
-                        src={outfitSections.Accessories.imageURL}
-                        alt={outfitSections.Accessories.name}
-                        className="rounded-full w-8 h-8 object-cover"
-                      />
-                    ) : (
-                      <FiWatch color="white" size={30} />
-                    )}
-                  </button>
-                </div>
-                <div className="flex flex-row gap-15 mr-35">
-                  <button
-                    onClick={() => handleSelectCategory("Outerwear")}
-                    className="bg-white/45 px-6 py-10 border-2 border-surface border-dashed rounded-2xl"
-                  >
-                    {ClothesLoading ? (
-                      <div className="border-white border-b-2 rounded-full w-8 h-8 animate-spin"></div>
-                    ) : outfitSections.Outerwear ? (
-                      <img
-                        src={outfitSections.Outerwear.imageURL}
-                        alt={outfitSections.Outerwear.name}
-                        className="rounded-full w-8 h-8 object-cover"
-                      />
-                    ) : (
-                      <TbJacket color="white" size={30} />
-                    )}
-                  </button>
-                  <button
-                    onClick={() => handleSelectCategory("Tops")}
-                    className="bg-white/45 px-6 py-10 border-2 border-surface border-dashed rounded-2xl"
-                  >
-                    {ClothesLoading ? (
-                      <div className="border-white border-b-2 rounded-full w-8 h-8 animate-spin"></div>
-                    ) : outfitSections.Tops ? (
-                      <img
-                        src={outfitSections.Tops.imageURL}
-                        alt={outfitSections.Tops.name}
-                        className="rounded-full w-8 h-8 object-cover"
-                      />
-                    ) : (
-                      <IoShirtOutline color="white" size={30} />
-                    )}
-                  </button>
-                </div>
-                <button
-                  onClick={() => handleSelectCategory("Bottoms")}
-                  className="bg-white/45 px-6 py-10 border-2 border-surface border-dashed rounded-2xl"
-                >
-                  {ClothesLoading ? (
-                    <div className="border-white border-b-2 rounded-full w-8 h-8 animate-spin"></div>
-                  ) : outfitSections.Bottoms ? (
-                    <img
-                      src={outfitSections.Bottoms.imageURL}
-                      alt={outfitSections.Bottoms.name}
-                      className="rounded-full w-8 h-8 object-cover"
-                    />
-                  ) : (
-                    <PiPants color="white" size={30} />
-                  )}
-                </button>
-                <button
-                  onClick={() => handleSelectCategory("Feet")}
-                  className="bg-white/45 p-6 border-2 border-surface border-dashed rounded-2xl"
-                >
-                  {ClothesLoading ? (
-                    <div className="border-white border-b-2 rounded-full w-8 h-8 animate-spin"></div>
-                  ) : outfitSections.Feet ? (
-                    <img
-                      src={outfitSections.Feet.imageURL}
-                      alt={outfitSections.Feet.name}
-                      className="rounded-full w-8 h-8 object-cover"
-                    />
-                  ) : (
-                    <TbShoe color="white" size={30} />
-                  )}
-                </button>
-              </div>
-            </article>
+            <OutfitPickerSection
+              handleSelectCategory={handleSelectCategory}
+              outfitSections={outfitSections}
+              ClothesLoading={ClothesLoading}
+            />
             {selectedCategory && (
-              <section className="relative flex flex-col flex-1 bg-surface px-5 rounded-2xl max-w-[65%] h-full max-h-[530px] overflow-y-scroll scrollbar-thin-gray">
-                {ClothesLoading ? (
-                  <LoadingTagsFilter />
-                ) : (
-                  tags && (
-                    <TagsFilter
-                      tags={tags}
-                      tagsContainerRef={tagsContainerRef}
-                      handleTagClick={filterItemsByTag}
-                      selectedTag={selectedTag}
-                    />
-                  )
-                )}
-                {filteredItems.length === 0 ? (
-                  <p className="flex justify-center items-center col-span-3 mt-15 w-full text-muted text-xl lg:text-3xl text-center">
-                    No items found in this category
-                  </p>
-                ) : (
-                  <div className="gap-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12">
-                    {filteredItems.map((item: WardrobeItem) => (
-                      <ItemCard
-                        key={item.itemId}
-                        item={item}
-                        onClick={() => handleItemClick(item)}
-                      />
-                    ))}
-                  </div>
-                )}
-              </section>
+              <AsideClothingPickerSection
+                tagsContainerRef={tagsContainerRef}
+                Clothes={ClothesResult.data}
+                ClothesLoading={ClothesLoading}
+                setSelectedTag={setSelectedTag}
+                selectedTag={selectedTag}
+                tags={tags}
+                setFilteredItems={setFilteredItems}
+                filteredItems={filteredItems}
+                setSelectedCategory={setSelectedCategory}
+                selectedCategory={selectedCategory}
+                handleItemClick={handleItemClick}
+              />
             )}
           </section>
-          {showUpdateButton && (
+          <div className="flex lg:flex-row flex-col justify-center items-center gap-4 mt-5">
+            {showUpdateButton && (
+              <Button
+                version="v1"
+                type="button"
+                bgColor="secondary"
+                textColor="title"
+                size="lg"
+                className="px-10 py-3 rounded-2xl"
+                onClick={validateAndUpdateOutfit}
+                disabled={OutfitLoading || ClothesLoading}
+              >
+                Update Outfit
+              </Button>
+            )}
             <Button
-              version="v1"
+              version="v2"
               type="button"
               bgColor="primary"
-              textColor="title"
+              textColor="primary"
               size="lg"
-              className="mt-2 px-10 py-3 rounded-2xl"
-              onClick={validateAndUpdateOutfit}
+              className="px-10 py-3 rounded-2xl"
+              onClick={() => setShowDeletePopUp(true)}
               disabled={OutfitLoading || ClothesLoading}
             >
-              Update Outfit
+              Delete Outfit
             </Button>
-          )}
-          <Button
-            version="v1"
-            type="button"
-            bgColor="primary"
-            textColor="title"
-            size="lg"
-            className="mt-2 px-10 py-3 rounded-2xl"
-            onClick={() => setShowDeletePopUp(true)}
-            disabled={OutfitLoading || ClothesLoading}
-          >
-            Delete Outfit
-          </Button>
+          </div>
         </>
       )}
     </main>

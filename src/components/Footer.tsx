07b049bc8b8e29b3eb668d9c3182b6e60851a00e
Victor-starr <v94625230@gmail.com>
2025-10-16 00:09:36 +0300

    Fix: Reduce Footer Height

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { FaGithub } from "react-icons/fa";
 import { Link } from "react-router";
 const Footer = () => {
   return (
-    <footer className="bottom-0 left-0 fixed flex justify-center items-center bg-card px-10 py-5 w-screen text-md text-white md:text-lg lg:text-xl">
+    <footer className="bottom-0 left-0 fixed flex justify-center items-center bg-card px-10 py-2 w-screen text-md text-white lg:text-lg">
       <Link
         to="https://github.com/Victor-starr/OutfitX"
         target="_blank"
